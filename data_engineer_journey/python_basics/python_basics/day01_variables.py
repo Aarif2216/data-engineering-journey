0634ba100fb49557b42c6f4cbb4128e407a20898
MD ARIF HUSSAIN <arifussain118@gmail.com>
2026-01-27 21:35:26 +0530

    Day 1: Python variables and basic operations

--- data_engineer_journey/python_basics/python_basics/day01_variables.py
+++ data_engineer_journey/python_basics/python_basics/day01_variables.py
@@ -1,1 +1,11 @@
 print("Hello World!")
+name = "MD Arif Hussain"
+age = 22
+role = "Aspiring Data Engineer"
+
+x = 10
+y = 5
+
+print(f"Name: {name}, Age: {age}, Role: {role}")
+print(f"Sum: {x + y}")
+print(type(name), type(age))
